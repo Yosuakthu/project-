9e3edf410821c7b106ad9fda6f88dce5226bb956
yosuakthu <yosuakatuhupapona@gmail.com>
2021-09-07 19:52:36 +0800

    installasi-awal

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Auth\User as AuthUser;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
+
+Route::resource('users', \App\Http\Controllers\UserController::class)
+    ->middleware('auth');
+
+Route::resource('profil', \App\Http\Controllers\profilController::class)
+    ->middleware('auth');
